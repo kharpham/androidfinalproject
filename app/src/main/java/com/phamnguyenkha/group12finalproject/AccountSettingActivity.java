75f3998f0b4636cc006503ab23f36779b2d99ee8
Admin <tranvannamntt@gmail.com>
2024-04-08 07:49:26 +0700

    updateUI_p1

--- app/src/main/java/com/phamnguyenkha/group12finalproject/AccountSettingActivity.java
+++ app/src/main/java/com/phamnguyenkha/group12finalproject/AccountSettingActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity3 extends AppCompatActivity {
+public class AccountSettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.account_setting);
     }
 }
