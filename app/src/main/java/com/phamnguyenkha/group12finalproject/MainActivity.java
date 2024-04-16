6313fbfbc470f5021132884bcd708e04a90ddedf
Khapham <khar22102003@gmail.com>
2024-04-16 13:16:31 +0700

    Kha update 16/04

--- app/src/main/java/com/phamnguyenkha/group12finalproject/MainActivity.java
+++ app/src/main/java/com/phamnguyenkha/group12finalproject/MainActivity.java
@@ -1,17 +1,67 @@
     package com.phamnguyenkha.group12finalproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.phamnguyenkha.adapters.CategoryAdapter;
 import com.phamnguyenkha.group12finalproject.databinding.ActivityMainBinding;
+import com.phamnguyenkha.models.Category;
+import com.phamnguyenkha.models.Product;
 
-    public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    ArrayList<Product> products = new ArrayList<>();
+    ArrayList<Category> categories = new ArrayList<>();
+    CategoryAdapter adapter;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        loadDataFromFireStore();
+        initAdapter();
+    }
+
+    private void loadDataFromFireStore() {
+        db.collection("category").orderBy("Id", Query.Direction.ASCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (error != null) {
+                            Log.e("Firestore error", error.getMessage());
+                            return;
+                        }
+                        for (DocumentChange dc : value.getDocumentChanges()) {
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
+                                int Id = ((Long) dc.getDocument().get("Id")).intValue();
+                                int ImagePath = getResources().getIdentifier((String) dc.getDocument().get("ImagePath"), "drawable", getPackageName());
+                                String CategoryName = (String) dc.getDocument().get("CategoryName");
+                                categories.add(new Category(Id, CategoryName, ImagePath));
+                            }
+                        }
+                        for (Category category : categories) {
+                            Log.i("Category", category.getId() + " - " + category.getCategoryName() + " - " + category.getImagePath());
+                        }
+                    }
+                });
+    }
+
+    private void initAdapter() {
+        adapter = new CategoryAdapter(MainActivity.this, R.layout.category_layout, categories);
+        binding.lvCats.setAdapter(adapter);
     }
 }
