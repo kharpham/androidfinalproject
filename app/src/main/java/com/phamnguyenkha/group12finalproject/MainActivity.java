bf26dce2e28643fbb650046c7e447393008873d1
Admin <tranvannamntt@gmail.com>
2024-04-23 12:44:38 +0700

    change

--- app/src/main/java/com/phamnguyenkha/group12finalproject/MainActivity.java
+++ app/src/main/java/com/phamnguyenkha/group12finalproject/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         loadDataFromFireStore();
 
+
         binding.lvCats.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
